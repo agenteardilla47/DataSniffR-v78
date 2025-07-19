013a557fc9fe0517393612db4b178d1d2de1d6ae
agenteardilla47 <ivalkenberg@yandex.com>
2025-07-19 01:10:36 -0400

    Update __init__.py

--- datasniffr/models/__init__.py
+++ datasniffr/models/__init__.py
@@ -1,7 +1,20 @@
-# DataSniffR Models - The $33 Singularity! 🐶💾
-from . import datasniffr_pocketflow_engine
-from . import email_preparation_system
-from . import employee_analytics_dashboard
-from . import human_ai_merger
-from . import live_data_guardian
-from . import natural_language_interface
+diff --git a/datasniffr/models/__init__.py b/datasniffr/models/__init__.py
+--- a/datasniffr/models/__init__.py
++++ b/datasniffr/models/__init__.py
+@@ -1,7 +1,9 @@
+-# DataSniffR Models - The $33 Singularity! 🐶💾
+-from . import datasniffr_pocketflow_engine
+-from . import email_preparation_system
+-from . import employee_analytics_dashboard
+-from . import human_ai_merger
+-from . import live_data_guardian
+-from . import natural_language_interface
++# DataSniffR Models - The $33 Singularity! 🐶💾
++from . import datasniffr_pocketflow_engine
++from . import email_preparation_system
++from . import employee_analytics_dashboard
++from . import human_ai_merger
++from . import live_data_guardian
++from . import natural_language_interface
++from . import company_vibe_analyzer
++
